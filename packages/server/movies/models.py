64b28d20040c5bc9851375ed07b79495df3e2bf3
Manyu Lakhotia <lakemonyew@gmail.com>
2022-02-12 18:24:25 -0600

    Fixes pk warning django movie

--- packages/server/movies/models.py
+++ packages/server/movies/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Movie(models.Model):
+  id = models.AutoField(primary_key=True)
   title = models.CharField(max_length=100)
   genre = models.CharField(max_length=100)
   year = models.IntegerField()
