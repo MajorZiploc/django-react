95f13dd7320bf795b4fe62382c7c9fd6795b464c
Manyu Lakhotia <lakemonyew@gmail.com>
2022-02-15 13:32:26 -0600

    Adds find conversion to mac/nonmac toggle

--- utils.bash
+++ utils.bash
@@ -4,7 +4,7 @@ function _just_to_nonmac {
   [[ -z "$just_project_root" ]] && { echo "Must specify just_project_root!" >&2; return 1; }
   local files=("${just_project_root}/just.bash" "${just_project_root}/packages/server/just.bash" "${just_project_root}/packages/client/just.bash");
   for file in ${files[@]}; do
-    sed -E -i'' 's/\bgsed\b/sed/g' "$file";
+    sed -E -i'' 's/\bgsed\b/sed/g;s/(find )-E (\S+ )(-mindepth \S+ )?(-maxdepth \S+ )?(-type \S+ )?/\1\2\3\4\5-regextype egrep /g;' "$file";
   done;
 }
 
@@ -14,7 +14,7 @@ function _just_to_mac {
   [[ -z "$just_project_root" ]] && { echo "Must specify just_project_root!" >&2; return 1; }
   local files=("${just_project_root}/just.bash" "${just_project_root}/packages/server/just.bash" "${just_project_root}/packages/client/just.bash");
   for file in ${files[@]}; do
-    gsed -E -i'' 's/\bsed\b/gsed/g' "$file";
+    gsed -E -i'' 's/\bsed\b/gsed/g;s/find(.*?) -regextype egrep/find -E\1/g;' "$file";
   done;
 }
 
