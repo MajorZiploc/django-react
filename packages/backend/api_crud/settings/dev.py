986f8ecf0f63c1aab62a530a96656698a632cd94
Manyu Lakhotia <lakemonyew@gmail.com>
2022-02-21 17:11:16 -0600

    Adds backend secret

--- packages/backend/api_crud/settings/dev.py
+++ packages/backend/api_crud/settings/dev.py
@@ -4,8 +4,7 @@ import os
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'SECRET_KEY'
+SECRET_KEY = os.environ['BACKEND_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
