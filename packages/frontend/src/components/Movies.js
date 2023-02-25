f679c0572217ac4966d6968be568e74afd2188fc
Manyu Lakhotia <lakemonyew@gmail.com>
2023-02-25 17:44:44 -0600

    Account for sticky header on pages

--- packages/frontend/src/components/Movies.js
+++ packages/frontend/src/components/Movies.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import DataContext from '../context/DataContext';
+import { useGlobalStyles } from '../utils';
 import GenericCrudTable from './GenericCrudTable';
 
 const Movies = () => {
   const data = React.useContext(DataContext);
+  const globalStyles = useGlobalStyles();
   const validatedMovie = enteredMovie => {
     var isValid = true;
     var errorMessage = '';
@@ -41,7 +43,12 @@ const Movies = () => {
     validatedModel: validatedMovie,
     tableId: 'moviesTable',
   };
-  return <GenericCrudTable {...crudTableProps} />;
+
+  return (
+    <div className={globalStyles.pageStyles}>
+      <GenericCrudTable {...crudTableProps} />
+    </div>
+  );
 };
 
 export default Movies;
