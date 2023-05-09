8a3f5cefc2d4125db945244b6ba778a8546c6211
Manyu Lakhotia <lakemonyew@gmail.com>
2023-05-09 18:05:53 -0500

    +rbql column_names query

--- db_queries/rbql.bash
+++ db_queries/rbql.bash
@@ -1,6 +1,8 @@
 # parse tables/*.csv files for unique table names
 rbql --with-header --query "Select distinct a.table_name" --delim ',' --policy quoted_rfc
 
-# parse tables/*.csv files for a table columns
+# parse tables/*.csv files for a table column_info
 rbql --with-header --query "Select a.* where a.table_name == 'table_name'" --delim ',' --policy quoted_rfc
 
+# parse tables/*.csv files for a table column_name
+rbql --with-header --query "Select a.column_name where a.table_name == 'table_name'" --delim ',' --policy quoted_rfc
