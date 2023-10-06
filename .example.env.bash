85f57498a313d04eec3be6347bbcbe41bac28265
Manyu Lakhotia <lakemonyew@gmail.com>
2023-10-06 13:45:39 -0500

    +REACT_APP env vars to bash file

--- .example.env.bash
+++ .example.env.bash
@@ -7,6 +7,8 @@ export JUST_UI_TESTS_EMAIL="abby_smith_testuserz@fakedomaintest1.com";
 export JUST_UI_TESTS_FIRST_NAME="Abby";
 export JUST_UI_TESTS_LAST_NAME="Smith";
 export JUST_UI_TESTS_ENV="local";
+export REACT_APP_PUBLIC_URL='http://127.0.0.1'
+export REACT_APP_BACKEND_PORT='8000'
 
 # These will be set in a pipline and do not need to be set locally unless you want to test deployments locally
 export AWS_ACCOUNT_ID='abc123';
