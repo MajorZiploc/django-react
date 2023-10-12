ccfe809c1c6ec6affb8a824dcf75e9793fa0c900
Manyu Lakhotia <lakemonyew@gmail.com>
2023-10-12 17:15:22 -0500

    cleanup rbql queries

--- db_queries/rbql.bash
+++ db_queries/rbql.bash
@@ -1,11 +1,8 @@
 # parse tables/*.csv files for unique table names
 rbql --with-header --query "Select distinct a.table_name" --delim ',' --policy quoted_rfc
 
-# parse tables/*.csv files for a table column_info
-rbql --with-header --query "Select a.* where a.table_name == 'table_name'" --delim ',' --policy quoted_rfc
-
-# parse tables/*.csv files for a table entry_name
-rbql --with-header --query "Select a.entry_name where a.table_name == 'table_name'" --delim ',' --policy quoted_rfc
+# parse tables/*.csv files for table column_info with table name like 'table_name'
+rbql --with-header --query "Select a.* where 'table_name' in a.table_name" --delim ',' --policy quoted_rfc
 
 # remove constraint info
 rbql --with-header --query "Select a.* where not a.entry_type != 'constraint'" --delim ',' --policy quoted_rfc
