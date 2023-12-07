a2089bca8ad81fe9eb92f6ea4ea97c1af30017d2
Manyu Lakhotia <lakemonyew@gmail.com>
2023-12-07 14:25:24 -0600

    improve env vars;use self managed pgdb

--- .example.env.bash
+++ .example.env.bash
@@ -9,6 +9,11 @@ export JUST_UI_TESTS_LAST_NAME="Smith";
 export JUST_UI_TESTS_ENV="local";
 export REACT_APP_PUBLIC_URL='http://127.0.0.1'
 export REACT_APP_BACKEND_PORT='8000'
+export PGDBLOCAL="/tmp/django-react-postgres-data/";
+export PGUSER='postgres';
+export PGPASSWORD='password';
+export PGPORT=5432;
+export PGDATABASE="postgres";
 
 # These will be set in a pipline and do not need to be set locally unless you want to test deployments locally
 export AWS_ACCOUNT_ID='abc123';
--- dev_tools/db_queries/update_tables.sh
+++ dev_tools/db_queries/update_tables.sh
@@ -4,6 +4,7 @@ function main {
 
   # from my dotfiles: https://github.com/MajorZiploc/dotfiles/blob/master/shared/required/home/.bashrc.d/portable/snippets.bash
   . ~/.bashrc.d/portable/snippets.bash;
+  . ../../.env.bash;
   local _command; _command="$(snip_sql_column_view_fn_info "pgsql" "low" "low" "none")";
 
   local container_name="django-react-db";
@@ -11,10 +12,6 @@ function main {
   local begin="BEGIN";
   local end="END";
   export PGHOST="127.0.0.1";
-  export PGPORT="5432";
-  export PGDATABASE="postgres";
-  export PGUSER="postgres";
-  export PGPASSWORD="password";
   echo "$padding $begin updating localhost $padding";
   local table_dir="./src/tables";
   mkdir -p "${table_dir}";
