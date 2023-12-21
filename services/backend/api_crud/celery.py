ea55acbfd78158155acd0bce804483a633213b27
Manyu Lakhotia <lakemonyew@gmail.com>
2023-12-21 12:50:33 -0600

    !celery broker to use rabbitmq

--- services/backend/api_crud/celery.py
+++ services/backend/api_crud/celery.py
@@ -3,7 +3,7 @@ from celery import Celery
 from api_crud import celeryconfig
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api_crud.settings.dev")
-app = Celery("api_crud")
+app = Celery("api_crud", broker=celeryconfig.broker_url)
 
 app.config_from_object(celeryconfig)
 app.autodiscover_tasks()  # Only autodiscovers tasks in files named tasks.py
