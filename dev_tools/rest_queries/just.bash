e1c43608b33dbecae4864b7b01374375a41d047d
Manyu Lakhotia <lakemonyew@gmail.com>
2023-12-27 22:03:04 -0600

    -& from replace patterns

--- dev_tools/rest_queries/just.bash
+++ dev_tools/rest_queries/just.bash
@@ -6,10 +6,10 @@ cd ~-;
 
 echo "NOTE: SOURCE THIS FILE WHILE INSIDE THE SAME PATH AS THIS FILE. ELSE YOU WILL HAVE INVALID BEHAVIOR";
 
-# NOTE: THIS WAS MAINLY COPIED FROM CHATGPT. NEEDS MORE TESTING
 function just_rest_url_encode {
-  echo "$1" | sed -E 's/%/%25/g; s/ /%20/g; s/!/%21/g; s/"/%22/g; s/#/%23/g; s/\$/%24/g; s/\&/%26/g; s/'"'"'/%27/g; s/\(/%28/g; s/\)/%29/g; s/\*/%2A/g; s/\+/%2B/g; s/,/%2C/g; s/\//%2F/g; s/:/%3A/g; s/;/%3B/g; s/\?/%3F/g; s/@/%40/g; s/\[/%5B/g; s/\\/%5C/g; s/\]/%5D/g; s/\^/%5E/g; s/_/%5F/g; s/`/%60/g; s/\{/%7B/g; s/\|/%7C/g; s/\}/%7D/g; s/~/%7E/g';
-  # s/=/%3D/g;
+  echo "$1" | sed -E 's/%/%25/g; s/ /%20/g; s/!/%21/g; s/"/%22/g; s/#/%23/g; s/\$/%24/g; s/'"'"'/%27/g; s/\(/%28/g; s/\)/%29/g; s/\*/%2A/g; s/\+/%2B/g; s/,/%2C/g; s/\//%2F/g; s/:/%3A/g; s/;/%3B/g; s/\?/%3F/g; s/@/%40/g; s/\[/%5B/g; s/\\/%5C/g; s/\]/%5D/g; s/\^/%5E/g; s/_/%5F/g; s/`/%60/g; s/\{/%7B/g; s/\|/%7C/g; s/\}/%7D/g; s/~/%7E/g';
+  # Removed patterns:
+  # s/=/%3D/g; s/\&/%26/g;
 }
 
 function just_rest_get_token {
