56452915c66ad260f4ac271548bec4ab45931dd7
Manyu Lakhotia <mlakhotia@conservis.ag>
2023-12-28 15:43:41 -0600

    init schedule_jobs

--- services/backend/integrations/models.py
+++ services/backend/integrations/models.py
@@ -2,6 +2,17 @@ import uuid
 from django.db import models
 from django.utils.timezone import now
 
+# TODO: Would be nice to have this in its own 'scheduler' app instead of being in 'integrations'
+class ScheduledJob(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    job_type = models.TextField()
+    next_scheduled = models.DateTimeField()
+    delay_seconds = models.IntegerField()
+    job_info = models.JSONField(default=dict)
+    last_ran = models.DateTimeField(null=True)
+    run_count = models.IntegerField(default=0)
+    delete_after_count = models.IntegerField(default=1)
+
 class AuditableModel(models.Model):
     created_date = models.DateTimeField(default=now, editable=False)
     updated_date = models.DateTimeField(auto_now=True)
--- services/backend/integrations/tasks/tasks.py
+++ services/backend/integrations/tasks/tasks.py
@@ -1,10 +1,13 @@
 from celery import (
   shared_task,
 )
+from django.utils.timezone import now, timedelta
+from integrations.models import ScheduledJob
+from api_crud import celeryconfig
 
 @shared_task(name="test_task")
-def test_task():
-    msg = "task worked"
+def test_task(*args, **kwargs):
+    msg = f"task worked and ran with args:{args}, kwargs:{kwargs}"
     print(msg)
     return msg
 
@@ -13,6 +16,22 @@ def delayed_task_watchdog():
     pass
 
 
+schedulable_tasks = {
+    'test_task': test_task,
+}
+
+# TODO: TEST
 @shared_task(name="schedule_jobs")
 def schedule_jobs():
-    pass
+    current_time = now()
+    jobs = ScheduledJob.objects.filter(next_scheduled__lte=current_time, next_scheduled__gte=current_time - timedelta(seconds=schedule_jobs_interval_seconds))
+    for job in jobs:
+        task = schedulable_tasks.get(job.job_type, None)
+        if not task: continue
+        task(**job.job_info)
+        job.next_scheduled = now() + timedelta(seconds=job.delayed_seconds or 0)
+        job.run_count = job.run_count + 1
+        if job.delete_after_count > job.run_count:
+            job.delete()
+        else:
+            job.save()
