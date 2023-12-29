88feae8e015fcddd0ec6c10d74bb1b442132c96c
Manyu Lakhotia <lakemonyew@gmail.com>
2023-12-29 06:31:54 -0600

    !url type for movies

--- services/backend/integrations/urls.py
+++ services/backend/integrations/urls.py
@@ -4,7 +4,7 @@ from integrations import views
 app_name = 'integrations'
 urlpatterns = [
     path('movies', views.ListCreateMovieAPIView.as_view(), name='get_post_movies'),
-    path('movies/<int:pk>', views.RetrieveUpdateDestroyMovieAPIView.as_view(), name='get_delete_update_movie'),
+    path('movies/<str:pk>', views.RetrieveUpdateDestroyMovieAPIView.as_view(), name='get_delete_update_movie'),
     path('movies/support', views.support_page, name='support_page'),
     path('movies/all', views.ListMovieAPIView.as_view(), name='list_movies'),
     path('movies/filter-data', views.MovieFilterDataAPIView.as_view(), name='movie_filter_data'),
