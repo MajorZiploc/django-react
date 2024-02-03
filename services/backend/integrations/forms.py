510fd2171f61ee8fd54ae96e8b468b46e4830935
Manyu Lakhotia <lakemonyew@gmail.com>
2024-02-03 16:06:46 -0600

    Make save slightly more efficient

--- services/backend/integrations/forms.py
+++ services/backend/integrations/forms.py
@@ -3,6 +3,7 @@ from django import forms
 
 class GenreForm(forms.Form):
     genre = forms.CharField(label="genre", max_length=100)
+    forloop_counter = forms.CharField(label="forloop_counter")
 
 class MovieForm(forms.Form):
     title = forms.CharField(label="title", max_length=100)
