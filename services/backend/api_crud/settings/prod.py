c18073192fcfec39065be90b1f574999ebbb19b7
Manyu Lakhotia <lakemonyew@gmail.com>
2024-06-21 15:23:58 -0500

    tmp less strict prod setting for testing

--- services/backend/api_crud/settings/prod.py
+++ services/backend/api_crud/settings/prod.py
@@ -2,7 +2,8 @@ from api_crud.settings.base import *
 import os
 import dj_database_url
 
-DEBUG = False
+# DEBUG = False
+DEBUG = True
 
 # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
@@ -11,10 +12,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # SECURITY WARNING: Should use CORS_ALLOWED_ORIGINS instead of these 2 things
-# CORS_ORIGIN_ALLOW_ALL = True
-# ALLOWED_HOSTS = ['*']
+CORS_ORIGIN_ALLOW_ALL = True
+ALLOWED_HOSTS = ['*']
 
-CORS_ALLOWED_ORIGINS = [os.environ['FRONTEND_PUBLIC_URL']]
+# CORS_ALLOWED_ORIGINS = [os.environ['FRONTEND_PUBLIC_URL']]
 
 DATABASES = {
     'default': dj_database_url.config(
