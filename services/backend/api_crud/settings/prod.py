c60de75d2c79dcc898a48bc12362f81074410f68
Manyu Lakhotia <lakemonyew@gmail.com>
2024-06-22 20:12:16 -0500

    ^cors settings

--- services/backend/api_crud/settings/prod.py
+++ services/backend/api_crud/settings/prod.py
@@ -11,10 +11,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # SECURITY WARNING: Should use CORS_ALLOWED_ORIGINS instead of these 2 things
-CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_ALLOW_ALL = True
 # ALLOWED_HOSTS = ['*']
 
-CORS_ALLOWED_ORIGINS = [os.environ['FRONTEND_PUBLIC_URL']]
+CORS_ALLOWED_ORIGIN_REGEXES = [rf'{os.environ["FRONTEND_PUBLIC_URL"]}']
 
 DATABASES = {
     'default': dj_database_url.config(
