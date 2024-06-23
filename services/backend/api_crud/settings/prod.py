06ab89ef1c960eb6a1702e21198f2a14c7b511e2
Manyu Lakhotia <lakemonyew@gmail.com>
2024-06-22 20:52:16 -0500

    ^cors settings

--- services/backend/api_crud/settings/prod.py
+++ services/backend/api_crud/settings/prod.py
@@ -14,7 +14,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # CORS_ORIGIN_ALLOW_ALL = True
 # ALLOWED_HOSTS = ['*']
 
-CORS_ALLOWED_ORIGINS = ['https://majorziploc-react.onrender.com/', 'https://django-react-z6z4.onrender.com/']
+CORS_ALLOWED_ORIGINS = ['https://majorziploc-react.onrender.com', 'https://django-react-z6z4.onrender.com']
 # CORS_ALLOWED_ORIGINS = [os.environ['FRONTEND_PUBLIC_URL']]
 
 CORS_ALLOW_METHODS = [
