9382798c954f5e31f69633cacb5fe9db6fdb5b2e
Manyu Lakhotia <lakemonyew@gmail.com>
2024-06-22 21:12:48 -0500

    -DB settings in base

--- services/backend/api_crud/settings/base.py
+++ services/backend/api_crud/settings/base.py
@@ -96,13 +96,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'api_crud.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
